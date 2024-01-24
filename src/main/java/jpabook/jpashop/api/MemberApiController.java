fde44d2661c8b3aa60638fa3b32618dd76794ddc
hyunwoo <khw4465@naver.com>
2024-01-24 11:06:07 +0900

    회원 조회 API

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,12 +19,40 @@ public class MemberApiController {
 
     private final MemberService memberService;
 
-    /** 회원 등록 API */
-    @PostMapping("/api/v1/members")
-    public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
-        Long id = memberService.join(member);
-        return new CreateMemberResponse(id);
+    /** 회원 조회 API */
+//    @GetMapping("/api/v1/members")
+//    public List<Member> membersV1() {
+//        return memberService.findMembers();
+//    }
+
+    @GetMapping("/api/v2/members")
+    public Result membersV2() {
+        List<Member> findMembers = memberService.findMembers();
+        List<MemberDto> collect = findMembers.stream()
+                .map(m -> new MemberDto(m.getName()))
+                .collect(Collectors.toList());
+
+        return new Result(collect);
     }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T> {
+        private T data;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto {
+        private String name;
+    }
+
+    /** 회원 등록 API */
+//    @PostMapping("/api/v1/members")
+//    public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {
+//        Long id = memberService.join(member);
+//        return new CreateMemberResponse(id);
+//    }
 
     @PostMapping("/api/v2/members")
     public CreateMemberResponse saveMemberV2(@RequestBody @Valid CreateMemberRequest request) {
