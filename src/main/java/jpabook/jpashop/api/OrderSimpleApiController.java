27821bcbce35b625d3cc2de031b8d89ad2d808e8
hyunwoo <khw4465@naver.com>
2024-01-26 22:08:59 +0900

    지연로딩 최적화3

--- src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -5,7 +5,6 @@ import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
-import jpabook.jpashop.service.OrderService;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -38,25 +37,35 @@ public class OrderSimpleApiController {
     }
 
     @GetMapping("/api/v2/simple-orders")
-    public List<SimpleOrderDto> ordersV2() {
+    public List<OrderSimpleQueryDto> ordersV2() {
         List<Order> orders = orderRepository.findAllByString(new OrderSearch());
 
-        List<SimpleOrderDto> result = orders.stream()
-                .map(o -> new SimpleOrderDto(o))
+        List<OrderSimpleQueryDto> result = orders.stream()
+                .map(o -> new OrderSimpleQueryDto(o))
+                .collect(Collectors.toList());
+
+        return result;
+    }
+
+    @GetMapping("/api/v3/simple-orders")
+    public List<OrderSimpleQueryDto> ordersV3() {
+        List<Order> orders = orderRepository.findAllWithMemberDelivery();
+        List<OrderSimpleQueryDto> result = orders.stream()
+                .map(o -> new OrderSimpleQueryDto(o))
                 .collect(Collectors.toList());
 
         return result;
     }
 
     @Data
-    static class SimpleOrderDto {
+    public class OrderSimpleQueryDto {
         private Long orderId;
         private String name;
         private LocalDateTime orderDate;
         private OrderStatus orderStatus;
         private Address address;
 
-        public SimpleOrderDto(Order order) {
+        public OrderSimpleQueryDto(Order order) {
             orderId = order.getId();
             name = order.getMember().getName(); // LAZY 초기화
             orderDate = order.getOrderDate();
@@ -64,4 +73,5 @@ public class OrderSimpleApiController {
             address = order.getDelivery().getAddress();
         }
     }
-}
+
+    }
