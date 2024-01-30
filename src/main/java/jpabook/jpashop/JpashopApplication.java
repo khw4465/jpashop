d3029098354bb59c76eddf979b0f9cab27c3982d
hyunwoo <khw4465@naver.com>
2024-01-30 22:03:48 +0900

    컬렉션조회 최적화1

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -10,10 +10,10 @@ public class JpashopApplication {
 
     public static void main(String[] args) { SpringApplication.run(JpashopApplication.class, args); }
 
-//    @Bean
-//    Hibernate5JakartaModule hibernate5JakartaModule() {
-//        Hibernate5JakartaModule hibernate5JakartaModule = new Hibernate5JakartaModule();
+    @Bean
+    Hibernate5JakartaModule hibernate5JakartaModule() {
+        Hibernate5JakartaModule hibernate5JakartaModule = new Hibernate5JakartaModule();
 //        hibernate5JakartaModule.configure(Hibernate5JakartaModule.Feature.FORCE_LAZY_LOADING, true);
-//        return hibernate5JakartaModule;
-//    }
+        return hibernate5JakartaModule;
+    }
 }
--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -3,11 +3,14 @@ package jpabook.jpashop.domain;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import jpabook.jpashop.domain.item.Item;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OrderItem {
 
     @Id @GeneratedValue
